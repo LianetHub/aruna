c94ed23668a44f97494cb55f41cb40ba171f2710
lianet <jack.tolmak@yandex.ru>
2025-05-22 17:39:31 +0300

    commit

--- src/js/modules/intInputMask.js
+++ src/js/modules/intInputMask.js
@@ -1,43 +1,35 @@
+
 import IMask from 'imask';
 import { codeArray } from './codes.js';
 
 export const intInputMask = () => {
-
-
-
-
     const inputs = document.querySelectorAll("input[type='tel']");
 
-
-
     inputs.forEach(input => {
-
         const mask = IMask(input, {
             mask: codeArray,
             dispatch: (appended, dynamicMasked) => {
                 const number = (dynamicMasked.value + appended).replace(/\D/g, '');
 
-                const firstChar = number.charAt(0);
-
-                if (firstChar === '7' || firstChar === '8' || firstChar === '9') {
-                    return dynamicMasked.compiledMasks.find(m => m.startsWith === "7");
-                }
 
                 return dynamicMasked.compiledMasks.find(m => number.indexOf(m.startsWith) === 0);
             }
+        });
 
-        })
-    })
-}
-// dispatch: (appended, dynamicMasked) => {
-//                 const number = (dynamicMasked.value + appended).replace(/\D/g, '');
+        input.addEventListener('focus', () => {
 
+            if (input.value.trim() === '') {
+                mask.value = '+7';
+            }
+        });
 
-//                 const firstChar = number.charAt(0);
-
-//                 if (firstChar === '7' || firstChar === '8' || firstChar === '9') {
-//                     return dynamicMasked.compiledMasks.find(m => m.startsWith === "7");
-//                 }
-
-//                 return dynamicMasked.compiledMasks.find(m => number.indexOf(m.startsWith) === 0)
-//             }
+        input.addEventListener('input', () => {
+            if (input.value === '8') {
+                mask.value = '+7';
+            }
+            if (input.value === '9') {
+                mask.value = '+79';
+            }
+        });
+    });
+};
